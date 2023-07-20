62cb5840d9a9d3137b6d6dade31d19817c249e8d
ApacheMoy <852591265@qq.com>
2023-07-21 00:12:49 +0800

    fix bug

--- assets/script/moveControl.ts
+++ assets/script/moveControl.ts
@@ -1,82 +1,76 @@
 
+import { VirtualInput } from './VirtualInput';
 const {ccclass, property} = cc._decorator;
+
 
 @ccclass
 export default class NewClass extends cc.Component {
-
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
-
-
-    /**
+/**
      * 手指部分
      */
     @property(cc.Sprite)
-    thumbnail: cc.Sprite | null = null;
+    thumbnail = null;
 
     /**
      * 摇杆的背景
      */
     @property(cc.Sprite)
-    joyStickBg: cc.Sprite | null = null;
+    joyStickBg = null;
 
-    @property
-    banJin:number=100;
+    /**
+     * 摇杆的半径
+     */
+    @property(cc.Float)
+    radius = 130;
 
-        /**
+    /**
      * 摇杆初始化的位置
      */
-    initJoyStickBgPosition: cc.Vec2 = cc.v2()
+    initJoyStickBgPosition = cc.v2();
 
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {}
-
-    start () {
+    onLoad() {
         this.node.on(cc.Node.EventType.TOUCH_MOVE, this.onTouchMove, this);
         this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
         this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
         this.node.on(cc.Node.EventType.TOUCH_START, this.onTouchStart, this);
-        this.initJoyStickBgPosition = this.getWorldPos(this.joyStickBg.node);
+        this.initJoyStickBgPosition = this.joyStickBg.node.getPosition();
     }
 
-    // update (dt) {}
-
-
-    getWorldPos(node:cc.Node):cc.Vec2{
-        return this.node.convertToWorldSpaceAR(cc.Vec2.ZERO);
-    }
-    setWorldPos(nodeL:cc.Node, pos:cc.Vec2){
-        this.node.setPosition(this.node.parent.convertToNodeSpaceAR(pos));
+    onDestroy() {
+        this.node.off(cc.Node.EventType.TOUCH_MOVE, this.onTouchMove, this);
+        this.node.off(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+        this.node.off(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+        this.node.off(cc.Node.EventType.TOUCH_START, this.onTouchStart, this);
     }
 
-    onTouchStart(eventTouch: cc.Event.EventTouch) {
-        let x = eventTouch.touch.getLocationX();
-        let y = eventTouch.touch.getLocationY();
-        this.setWorldPos(this.node,new cc.Vec2(x,y));
+    onTouchStart(eventTouch) {
+        console.debug("开始触摸");
+        let touch = eventTouch.touch;
+        let touchPos = this.node.convertToNodeSpaceAR(touch.getLocation());
+        this.joyStickBg.node.setPosition(touchPos);
     }
 
     /**
      * 触摸移动
      * @param touchEvent 
      */
-    onTouchMove(touchEvent: cc.Event.EventTouch) {
+    onTouchMove(touchEvent) {
+        
         // 获取摇杆在 UI 的位置
-        let x = touchEvent.touch.getLocationX();
-        let y = touchEvent.touch.getLocationY();
+        let touch = touchEvent.touch;
+        let x = touch.getLocationX();
+        let y = touch.getLocationY();
 
         let worldPosition = new cc.Vec2(x, y);
         let localPosition = cc.v2();
 
         // 转化摇杆的位置到背景图的本地坐标
-        this.joyStickBg.node.inverseTransformPoint(localPosition, worldPosition);
+        this.joyStickBg.node.parent.convertToNodeSpaceAR(worldPosition, localPosition);
         let thumbnailPosition = cc.v2();
-        let len = localPosition.length();
-        localPosition.normalize();
-        cc.Vec2.scaleAndAdd(thumbnailPosition, cc.v2(), localPosition, Math.clamp(len, 0, this.radius));
+        let len = localPosition.mag();
+        localPosition.normalizeSelf();
+        console.debug("len="+len);
+        cc.Vec2.scaleAndAdd(thumbnailPosition, cc.v2(), localPosition, cc.misc.clampf(len, 0, this.radius));
 
         this.thumbnail.node.setPosition(thumbnailPosition);
 
@@ -90,12 +84,13 @@ export default class NewClass extends cc.Component {
      * 触摸结束
      * @param touchEvent 
      */
-    onTouchEnd(touchEvent: cc.Event.EventTouch) {
+    onTouchEnd(touchEvent) {
+        console.debug("触摸结束");
         this.thumbnail.node.setPosition(cc.v2());
         VirtualInput.horizontal = 0;
         VirtualInput.vertical = 0;
 
         // 摇杆的位置回归到初始化位置
-        this.joyStickBg.node.worldPosition = this.initJoyStickBgPosition;
+        this.joyStickBg.node.setPosition(this.initJoyStickBgPosition);
     }
 }
